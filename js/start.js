5ef612dbd4d42af13d6b9ad35ddc3308e7a336c4
natebc72 <nbchristiansen72@gmail.com>
2022-04-23 16:16:53 -0700

    updates for homepage

---
+++ js/start.js
@@ -0,0 +1,2 @@
+const options = {weekday: 'short', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
