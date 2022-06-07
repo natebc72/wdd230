52b8a5c5271e28a14236f0720ad2f0c52c429f46
natebc72 <nbchristiansen72@gmail.com>
2022-06-06 20:16:52 -0700

    updates

--- wk-4/js/hmpg.js
+++ wk-4/js/hmpg.js
@@ -1,13 +1,31 @@
+//this is for the date banner in header//
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+
+//this is the function for the begining of the week banner//
+let d = new Date().getDay();
+const meet = document.getElementById("meet");
+if (d == 1 || d ==2) {
+	meet.style.display = "block";
+}
+//this is for the banner button to close it//
+const sheew = document.querySelector("#sheew");
+sheew.addEventListener("click", () =>{
+	meet.style.display = "none";
+});
+
+//this functiion is for the hamburger menu
+function toggleMenu(){
+	document.getElementById("navigation").classList.toggle("open");
+    document.getElementById("drop").classList.toggle("open");
+}
+
+const x = document.getElementById("drop")
+x.onclick = toggleMenu;
+
+
+//this is for the last modified in footer
 document.querySelector(
 	"#lastModified"
 ).textContent = `Last Modification: ${document.lastModified}`;
-
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-// To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-
 
